d0c0991fd57f7ed4d936fc5dc7ed508e0b80b5a2
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2021-01-11 00:04:42 -0300

    Setup project

---
+++ pages/_app.tsx
@@ -0,0 +1,33 @@
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import React, { useEffect } from 'react';
+
+import theme from '../theme';
+
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>My page</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+  );
+};
+
+export default App;
---
+++ pages/_document.tsx
@@ -0,0 +1,53 @@
+import { ServerStyleSheets } from '@material-ui/core';
+import React from 'react';
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
+
+import theme from '../theme';
+
+class AppDocument extends Document {
+  public render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="theme-color" content={theme.palette.primary.main} />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+AppDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
+};
+
+export default AppDocument;
